12d5930440c1d78833cdb31f91511b0783c5df2f
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-21 21:33:36 +0800

    Add files via upload

---
+++ data/point_2.js
@@ -0,0 +1,12 @@
+var json_point_2 = {
+"type": "FeatureCollection",
+"name": "point_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Jeep Merapi Adventure M Jak", "desc": "Jeep Merapi Adventure M Jak, Gondang, Umbulharjo, Cangkringan, Sleman Regency, Special Region of Yogyakarta 55583, Indonesia", "x": "110.4341921", "y": "-7.6141998", "no": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.4341921, -7.6141998 ] } },
+{ "type": "Feature", "properties": { "name": "Museum Mini Sisa Hartaku", "desc": "Museum Mini Sisa Hartaku, Jl. Petung Merapi, Petung, Kepuharjo, Kec. Cangkringan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55583, Indonesia", "x": "110.4490782", "y": "-7.6005361", "no": "2" }, "geometry": { "type": "Point", "coordinates": [ 110.4490782, -7.6005361 ] } },
+{ "type": "Feature", "properties": { "name": "Bunker Kaliadem Merapi", "desc": "Bunker Kaliadem Merapi, Kinarejo, Hargobinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "x": "110.4476821", "y": "-7.5829333", "no": "3" }, "geometry": { "type": "Point", "coordinates": [ 110.4476821, -7.5829333 ] } },
+{ "type": "Feature", "properties": { "name": "Malioboro Jogjakarta", "desc": "MALIOBORO JOGJAKARTA, Jl. Malioboro, Sosromenduran, Gedong Tengen, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55271, Indonesia", "x": "110.365843", "y": "-7.7925764", "no": "4" }, "geometry": { "type": "Point", "coordinates": [ 110.365843, -7.7925764 ] } },
+{ "type": "Feature", "properties": { "name": "Just Playon", "desc": "Just Playon, Jl. Prawirotaman No.3, Brontokusuman, Kec. Mergangsan, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55153, Indonesia", "x": "110.3687897", "y": "-7.8185146", "no": "5" }, "geometry": { "type": "Point", "coordinates": [ 110.3687897, -7.8185146 ] } }
+]
+}
